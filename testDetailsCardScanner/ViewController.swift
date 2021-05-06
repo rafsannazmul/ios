2d346a9ee41c1f109a9646969fe2b7186d3daedd
Rafsan <rafsannazmul@hotmail.com>
2021-05-06 11:12:58 +0600

    Initial Commit

---
+++ testDetailsCardScanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testDetailsCardScanner
+//
+//  Created by odysseyMacPro1 on 5/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
